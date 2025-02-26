b7647b80c2952c8e056b766013bc909d582df464
kumar <krksingh.99@gmail.com>
2025-02-27 02:40:16 +0530

    Chore: Removed unused image

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+//tsconfigPaths is used for path aliases
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
